8f5facab1a7565c217f15cd85948abddbeadfc9a
Jose Pena <yadose@gmail.com>
2017-07-05 10:33:28 +0200

    working state with login

--- angular4/src/app/login/login.component.ts
+++ angular4/src/app/login/login.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  sName: string;
+  sPword: string;
+  sSubmit: string;
+  constructor() {
+    this.sName = 'name';
+    this.sPword = 'password';
+    this.sSubmit = 'send';
+  }
 
   ngOnInit() {
   }
 
+  sendLogin(){
+    //login function needs to be implemented later on
+
+  }
 }
