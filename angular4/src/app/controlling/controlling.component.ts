34d0a252323101ba024769e19755666a836a033f
Jose Pena <yadose@gmail.com>
2017-07-05 09:13:16 +0200

    added component controlling

---
+++ angular4/src/app/controlling/controlling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-controlling',
+  templateUrl: './controlling.component.html',
+  styleUrls: ['./controlling.component.css']
+})
+export class ControllingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
