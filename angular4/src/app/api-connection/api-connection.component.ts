c1d9011dd7df5b76be5e27297044d37747fcf2ed
Jose Pena <yadose@gmail.com>
2017-07-07 12:10:26 +0200

    added outpout eventemitter to send json object as result

--- angular4/src/app/api-connection/api-connection.component.ts
+++ angular4/src/app/api-connection/api-connection.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Http,Response } from '@angular/http';
 
 @Component({
   selector: 'app-api-connection',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api-connection.component.css']
 })
 export class ApiConnectionComponent implements OnInit {
+  @Input() sTablename: string;
+  @Output() onLoadingComplete: EventEmitter<object>;
 
-  constructor() { }
+  sUrl: string;
+  oData: Object;
+  bLoading: boolean;
+  oHttp: Http;
+
+  constructor(http: Http) {
+    this.oHttp = http;
+    this.sUrl = 'http://localhost:8080/api/v1/';
+    this.onLoadingComplete = new EventEmitter();
+  }
 
   ngOnInit() {
+    this.makeRequest();
+  }
+
+  makeRequest() {
+    this.bLoading = true;
+    this.oHttp.request(this.sUrl+this.sTablename)
+    .subscribe((res: Response) => {
+      this.oData = res.json();
+      this.bLoading = false;
+      this.onLoadingComplete.emit(this.oData);
+    }
+    );
   }
 
 }
