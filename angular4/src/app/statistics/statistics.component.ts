6c8fcd50cb079d5f043c38a1d686f686230b1389
Jose Pena <yadose@gmail.com>
2017-07-11 15:18:49 +0200

    added listener for onready state on ngx table, also added new filter variable with handle

--- angular4/src/app/statistics/statistics.component.ts
+++ angular4/src/app/statistics/statistics.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
-
-
+import { HostListener } from '@angular/core';
 
 
 @Component({
@@ -10,18 +9,54 @@ import * as $ from 'jquery';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
-
+  @HostListener("document:keypress", ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent){
+    if(event.srcElement.id.length > 0 && event.srcElement.id == 'filterInput' && event.key == 'Enter'){
+      //get text from filter input after pressing enter
+      this.sendFilteredRequest($('#'+event.srcElement.id).val());
+    }
+  }
   sType: string;
-  jRows: object; //jsonstring
-  jColumns: object; //jsonstring
+  sFilter: string;
+  jRows: any; //json array of objects
+  jColumns: any; //json array of objects
+  sActiveFilter: string;
 
   constructor() {
     this.sType = 'job';
+    this.sActiveFilter = '';
+    this.sFilter = '';
+
   }
 
   ngOnInit() {
+
+  }
+
+  setType(siType){
+    //this.sType = siType;
+    this.sType = 'error';
+  }
+
+  getData(output){
+    this.jColumns = [];
+    //get columnnames
+    for(var inner in output[0]){
+      this.jColumns.push({name:inner});
+    }
+    this.jRows = output;
+    this.setFilterListener();
+
+  }
+
+  setFilterType(title){
+    $('#filterInput').attr('placeholder', title);
+    this.sActiveFilter = title.replace(/(\r\n|\n|\r)/gm,"").trim();
+  }
+
+  setFilterListener(){
     let self = this;
-    //disable contextmenu and set filtertype
+    //disable contextmenu and set filtertype listener
     $('datatable-header-cell').ready(function(){
       $('datatable-header-cell').on('contextmenu',function(){
         self.setFilterType($(this)[0].innerText);
@@ -30,15 +65,9 @@ export class StatisticsComponent implements OnInit {
     });
   }
 
-  setType(siType){
-    this.sType = siType;
-  }
-  getData(output){
-    this.jRows = output;
-    this.jColumns = [{ name: 'deviceId'}, {name: 'end'}, {name: 'pid'}, {name: 'start'}, {name: 'type'}, {name: 'updatedAt'}];
-  }
+  sendFilteredRequest(filterText){
+    console.log('new request');
+    this.sFilter = filterText;
 
-  setFilterType(title){
-    $('#filterInput').attr('placeholder', title);
   }
 }
