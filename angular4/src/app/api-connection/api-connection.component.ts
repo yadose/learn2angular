37d761568238c83206139d1a69c70835e0ad218f
Jose Pena <yadose@gmail.com>
2017-07-11 17:24:04 +0200

    refactored old function to GET and added new POST function and parameters for filter requests

--- angular4/src/app/api-connection/api-connection.component.ts
+++ angular4/src/app/api-connection/api-connection.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Http,Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-api-connection',
@@ -10,7 +10,10 @@ export class ApiConnectionComponent implements OnInit {
   @Input() set sFilter(val: string) {
     this._sFilter = val;
     if(val != ''){
-      this.makeRequest();
+      this.makeRequestPOST();
+    }
+    else if(typeof this.sTablename!='undefined'){
+      this.makeRequestGET();
     }
   }
   @Input() sTablename: string;
@@ -30,15 +33,38 @@ export class ApiConnectionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.makeRequest();
+    this.makeRequestGET();
   }
 
-  makeRequest() {
-
+  makeRequestGET() {
     this.bLoading = true;
     this.oHttp.request(this.sUrl+this.sTablename)
     .subscribe((res: Response) => {
       this.oData = JSON.parse(res.json());
+      this.bLoading = false;
+      this.onLoadingComplete.emit(this.oData);
+    }
+    );
+  }
+
+  makeRequestPOST() {
+    let body = '------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"filter\"\r\n\r\n'+ this.sActiveFilter +'\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"filtertext\"\r\n\r\n'+ this._sFilter +'\r\n';
+    let headerdata = new Headers();
+    headerdata.append('Content-Type','multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
+    this.bLoading = true;
+    this.oHttp.post(
+    this.sUrl+this.sTablename+'/filter/',
+    body,
+    {
+      headers: headerdata
+    })
+    .subscribe((res: Response) => {
+      try {
+        this.oData = JSON.parse(res.json());
+      }
+      catch (e){
+        this.oData = [{result:'No Result'}];
+      }
       this.bLoading = false;
       this.onLoadingComplete.emit(this.oData);
     }
